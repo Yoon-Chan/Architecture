1b84af64013f4ac829ad8486216adf60c40e71d4
윤찬 <vsvx13@naver.com>
2023-09-13 22:38:46 +0900

    feat: 뷰 바인딩 설정

---
+++ project/app/src/main/java/com/example/project/FirstFragment.kt
@@ -0,0 +1,38 @@
+package com.example.project
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.project.databinding.FragmentFirstBinding
+
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        //여기서 binding 기능 구현
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
